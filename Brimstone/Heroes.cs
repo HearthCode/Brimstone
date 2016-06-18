4fdb35b91a493be06072059d0c2e5756154a858a
Katy Coe <djkaty@start.no>
2016-06-18 19:58:17 +0200

    Add Hero class helpers

---
+++ Brimstone/Heroes.cs
@@ -0,0 +1,31 @@
+﻿namespace Brimstone
+{
+	public static class HeroClass {
+		public static Card Warrior = Cards.Find["HERO_01"];
+		public static Card Shaman = Cards.Find["HERO_02"];
+		public static Card Rogue = Cards.Find["HERO_03"];
+		public static Card Paladin = Cards.Find["HERO_04"];
+		public static Card Hunter = Cards.Find["HERO_05"];
+		public static Card Druid = Cards.Find["HERO_06"];
+		public static Card Warlock = Cards.Find["HERO_07"];
+		public static Card Mage = Cards.Find["HERO_08"];
+		public static Card Priest = Cards.Find["HERO_09"];
+	}
+
+	public static class SpecificHero {
+		public static Card Grommash = Cards.Find["HERO_01"];
+		public static Card Magni = Cards.Find["HERO_01a"];
+		public static Card Thrall = Cards.Find["HERO_02"];
+		public static Card Valeera = Cards.Find["HERO_03"];
+		public static Card Uther = Cards.Find["HERO_04"];
+		public static Card Liadrin = Cards.Find["HERO_04a"];
+		public static Card Rexxar = Cards.Find["HERO_05"];
+		public static Card Alleria = Cards.Find["HERO_05a"];
+		public static Card Malfurion = Cards.Find["HERO_06"];
+		public static Card Guldan = Cards.Find["HERO_07"];
+		public static Card Jaina = Cards.Find["HERO_08"];
+		public static Card Medivh = Cards.Find["HERO_08a"];
+		public static Card Khadgar = Cards.Find["HERO_08b"];
+		public static Card Anduin = Cards.Find["HERO_09"];
+	}
+}
