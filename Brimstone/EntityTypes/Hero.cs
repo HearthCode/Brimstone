9f912f77d0db7429a84858e0bcebb57b8879a76b
Katy Coe <djkaty@start.no>
2016-06-18 20:35:42 +0200

    Set each player's HERO_ENTITY on game start

--- Brimstone/EntityTypes/Hero.cs
+++ Brimstone/EntityTypes/Hero.cs
@@ -6,7 +6,10 @@ namespace Brimstone
 	public partial class Hero : CanBeDamaged
 	{
 		public Hero(Hero cloneFrom) : base(cloneFrom) { }
-		public Hero(Game game, IEntity controller, Card card, Dictionary<GameTag, int> tags = null) : base(game, controller, card, tags) { }
+		public Hero(Game game, IEntity controller, Card card, Dictionary<GameTag, int> tags = null) : base(game, controller, card, tags) {
+			// Set player's hero entity tag
+			controller[GameTag.HERO_ENTITY] = Id;
+		}
 
 		public override object Clone() {
 			return new Hero(this);
