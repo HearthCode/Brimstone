dc87d653d7309482a130f3c19b8220eb77b74eaa
Katy Coe <djkaty@start.no>
2016-06-19 14:57:18 +0200

    Generic RNG selector

--- Brimstone/Utils.cs
+++ Brimstone/Utils.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Brimstone
 {
-	public static class RNG
-	{
+	public static class RNG {
 		private static Random random = new Random();
 
 		public static int Between(int min, int max) {
 			return random.Next(min, max + 1);
+		}
+	}
+
+	public static class RNG<T> {
+		public static T Choose(List<T> choices) {
+			return choices[RNG.Between(0, choices.Count)];
 		}
 	}
 }
