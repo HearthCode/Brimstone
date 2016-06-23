90abd97f1d15ef87d0b5cedfcda8359191624563
Katy Coe <djkaty@start.no>
2016-06-23 18:22:53 +0200

    Add CreateMulligan and Select action factories

--- Brimstone/Behaviours.cs
+++ Brimstone/Behaviours.cs
@@ -1,4 +1,7 @@
-﻿namespace Brimstone
+﻿using System;
+using System.Collections.Generic;
+
+namespace Brimstone
 {
 	public class Behaviour
 	{
@@ -11,13 +14,19 @@
 	{
 		// Factory functions for DSL syntax
 		public static QueueAction BeginTurn { get { return new BeginTurn(); } }
+
 		public static ActionGraph Draw(ActionGraph player) { return new Draw { Args = { player } }; }
 		public static ActionGraph Give(ActionGraph player, ActionGraph card) { return new Give { Args = { player, card } }; }
 		public static ActionGraph Play(ActionGraph player, ActionGraph entity) { return new Play { Args = { player, entity } }; }
 
+		public static ActionGraph CreateMulligan(ActionGraph player) { return Select(player, p => ((Player)p).Hand.Slice(1, ((Player)p).NumCardsDrawnThisTurn)); }
+
 		public static QueueAction RandomOpponentMinion { get { return new RandomOpponentMinion(); } }
 		public static QueueAction AllMinions { get { return new AllMinions(); } }
 		public static ActionGraph RandomAmount(ActionGraph min, ActionGraph max) { return new RandomAmount { Args = { min, max } }; }
+
+		public static QueueAction Select(ActionGraph source, Func<IEntity, List<IEntity>> selector) { return new Selector { Args = { source }, Lambda = selector }; }
+
 		public static ActionGraph Damage(ActionGraph target, ActionGraph amount) { return new Damage { Args = { target, amount } }; }
 	}
 }
