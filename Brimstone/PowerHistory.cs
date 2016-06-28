b6c02b4fbc684de4cafc479ba58356bc4b6aa27e
Katy Coe <djkaty@start.no>
2016-06-28 02:59:18 +0200

    Add CreateEntity equality and IEquatable<T>

--- Brimstone/PowerHistory.cs
+++ Brimstone/PowerHistory.cs
@@ -66,9 +66,9 @@ namespace Brimstone
 		}
 	}
 
-	public class CreateEntity : PowerAction
+	public class CreateEntity : PowerAction, IEquatable<CreateEntity>
 	{
-		public Dictionary<GameTag, int> Tags { get; set; }
+		public Dictionary<GameTag, int> Tags { get; }
 
 		public CreateEntity(IEntity e) : base(e) {
 			Tags = new Dictionary<GameTag, int>();
@@ -86,6 +86,51 @@ namespace Brimstone
 				s += "\n    " + new Tag(t.Key, t.Value);
 			}
 			return s;
+		}
+
+		public static bool operator ==(CreateEntity x, CreateEntity y) {
+			if (ReferenceEquals(x, null))
+				return false;
+			return x.Equals(y);
+		}
+
+		public static bool operator !=(CreateEntity x, CreateEntity y) {
+			return !(x == y);
+		}
+
+		public override bool Equals(object o) {
+			if (!(o is CreateEntity))
+				return false;
+			return Equals((CreateEntity)o);
+		}
+
+		public bool Equals(CreateEntity o) {
+			if (ReferenceEquals(o, null))
+				return false;
+			if (ReferenceEquals(this, o))
+				return true;
+			// Must be same entity ID - we might want to change this later?
+			if (EntityId != o.EntityId)
+				return false;
+			// Same number of tags in each entity
+			if (Tags.Count != o.Tags.Count)
+				return false;
+			// Same tags must be present
+			foreach (var tagName in Tags.Keys)
+				if (!o.Tags.ContainsKey(tagName))
+					return false;
+			// Tags must have same tag values
+			foreach (var tagName in Tags.Keys)
+				if (Tags[tagName] != o.Tags[tagName])
+					return false;
+			return true;
+		}
+
+		public override int GetHashCode() {
+			int hash = (17 * 31 + EntityId);
+			foreach (var tag in Tags)
+				hash = (hash * 31 + (int)tag.Key) * 31 + tag.Value;
+			return hash;
 		}
 	}
 
