756997096779844e51cf03a1f45966feb225af54
Katy Coe <djkaty@start.no>
2016-08-14 00:58:57 +0200

    Add global settings class

---
+++ Brimstone/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brimstone
+{
+	public class Settings
+	{
+		public static bool CopyOnWrite = true;
+		public static bool ZoneCaching = true;
+		public static bool EntityHashCaching = true;
+		public static bool GameHashCaching = true;
+		public static bool PowerHistoryEquivalence = true;
+	}
+}
