527c385aea931c0b54c4063c9d95f1af01e6c333
Katy Coe <djkaty@start.no>
2016-08-13 22:38:36 +0200

    Add simple Benchmark suite

---
+++ Benchmarks/BenchmarkDefinitions.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Brimstone.Benchmark
+{
+	internal partial class Benchmarks {
+		// ======================================================
+		// =============== BENCHMARK DEFINITIONS ================
+		// ======================================================
+
+		public const int DefaultIterations = 100000;
+
+		public void LoadDefinitions() {
+			Tests = new Dictionary<string, Test>() {
+				{ "RawClone", new Test("Raw cloning speed (68 entities)", Test_RawClone) },
+				{ "BoomBotPreHit", new Test("Boom Bot pre-hit cloning test; 5 RC + 2 BB per side", Test_BoomBotPreHit) },
+				{ "BoomBotPreDeathrattle", new Test("Boom Bot pre-deathrattle cloning test; 5 RC + 2 BB per side", Test_BoomBotPreDeathrattle) },
+				{ "BoomBotUniqueStatesNS", new Test("Boom Bot hit; fuzzy unique states; Naive; 5 BR + 2 BB per side", Test_BoomBotUniqueStatesNS, Default_Setup2, 1) },
+				{ "BoomBotUniqueStatesDSF", new Test("Boom Bot hit; fuzzy unique states; DSF; 5 BR + 2 BB per side", Test_BoomBotUniqueStatesDSF, Default_Setup2, 1) },
+				{ "BoomBotUniqueStatesBSF", new Test("Boom Bot hit; fuzzy unique states; BSF; 5 BR + 2 BB per side", Test_BoomBotUniqueStatesBSF, Default_Setup2, 1) },
+				{ "ArcaneMissiles2UniqueStatesNS", new Test("Arcane Missiles (2); fuzzy unique game states; Naive; 5 BR + 2 BB per side", Test_2AMUniqueStatesNS, Default_Setup2, 1) },
+				{ "ArcaneMissiles2UniqueStatesDSF", new Test("Arcane Missiles (2); fuzzy unique game states; DSF; 5 BR + 2 BB per side", Test_2AMUniqueStatesDSF, Default_Setup2, 1) },
+				{ "ArcaneMissiles2UniqueStatesBSF", new Test("Arcane Missiles (2); fuzzy unique game states; BSF; 5 BR + 2 BB per side", Test_2AMUniqueStatesBSF, Default_Setup2, 1) },
+				{ "ArcaneMissiles3UniqueStatesDSF", new Test("Arcane Missiles (3); fuzzy unique game states; DSF; 5 BR + 2 BB per side", Test_3AMUniqueStatesDSF, Default_Setup2, 1) },
+				{ "ArcaneMissiles3UniqueStatesBSF", new Test("Arcane Missiles (3); fuzzy unique game states; BSF; 5 BR + 2 BB per side", Test_3AMUniqueStatesBSF, Default_Setup2, 1) },
+			};
+		}
+
+		// ======================================================
+		// ===============    BENCHMARK CODE    =================
+		// ======================================================
+
+		public static Game Default_Setup() {
+			return NewScenarioGame(MaxMinions: 7, NumBoomBots: 2, FillMinion: "River Crocolisk");
+		}
+		public static Game Default_Setup2() {
+			return NewScenarioGame(MaxMinions: 7, NumBoomBots: 2, FillMinion: "Bloodfen Raptor", FillDeck: false);
+		}
+
+		public void Test_RawClone(Game g, int it) {
+			for (int i = 0; i < it; i++)
+				g.CloneState();
+		}
+		public void Test_BoomBotPreHit(Game g, int it) {
+			var BoomBotId = g.Player1.Board.First(t => t.Card.Name == "Boom Bot").Id;
+			for (int i = 0; i < it; i++) {
+				Game cloned = (Game)g.CloneState();
+				((Minion)cloned.Entities[BoomBotId]).Hit(1);
+			}
+		}
+		public void Test_BoomBotPreDeathrattle(Game g, int it) {
+			// Capture after Boom Bot has died but before Deathrattle executes
+			var BoomBot = g.Player1.Board.First(t => t.Card.Name == "Boom Bot") as Minion;
+			g.ActionQueue.OnAction += (o, e) => {
+				ActionQueue queue = o as ActionQueue;
+				if (e.Action is Death && e.Source == BoomBot) {
+					for (int i = 0; i < it; i++) {
+						Game cloned = (Game)g.CloneState();
+						cloned.ActionQueue.ProcessAll();
+					}
+				}
+			};
+			BoomBot.Hit(1);
+		}
+
+		private void _boomBotUniqueStates(Game g, int it, ITreeSearcher search) {
+			var BoomBot = g.CurrentPlayer.Board.First(t => t.Card.Name == "Boom Bot") as Minion;
+			var tree = GameTree.BuildFor(
+				Game: g,
+				SearchMode: search,
+				Action: () => {
+					BoomBot.Hit(1);
+				}
+			);
+		}
+		public void Test_BoomBotUniqueStatesNS(Game g, int it) {
+			_boomBotUniqueStates(g, it, new NaiveTreeSearch());
+		}
+
+		public void Test_BoomBotUniqueStatesDSF(Game g, int it) {
+			_boomBotUniqueStates(g, it, new DepthFirstTreeSearch());
+		}
+
+		public void Test_BoomBotUniqueStatesBSF(Game g, int it) {
+			_boomBotUniqueStates(g, it, new BreadthFirstTreeSearch());
+		}
+
+		private void _missilesUniqueStates(Game g, int it, int missiles, ITreeSearcher search) {
+			Cards.FromName("Arcane Missiles").Behaviour.Battlecry = Actions.Damage(Actions.RandomOpponentCharacter, 1) * missiles;
+			Cards.FromName("Boom Bot").Behaviour.Deathrattle = Actions.Damage(Actions.RandomOpponentMinion, Actions.RandomAmount(1, 4));
+
+			var ArcaneMissiles = g.CurrentPlayer.Give("Arcane Missiles");
+			var tree = GameTree.BuildFor(
+				Game: g,
+				SearchMode: search,
+				Action: () => {
+					ArcaneMissiles.Play();
+				}
+			);
+		}
+
+		public void Test_2AMUniqueStatesNS(Game g, int it) {
+			_missilesUniqueStates(g, it, 2, new NaiveTreeSearch());
+		}
+
+		public void Test_2AMUniqueStatesDSF(Game g, int it) {
+			_missilesUniqueStates(g, it, 2, new DepthFirstTreeSearch());
+		}
+
+		public void Test_2AMUniqueStatesBSF(Game g, int it) {
+			_missilesUniqueStates(g, it, 2, new BreadthFirstTreeSearch());
+		}
+
+		public void Test_3AMUniqueStatesDSF(Game g, int it) {
+			_missilesUniqueStates(g, it, 3, new DepthFirstTreeSearch());
+		}
+
+		public void Test_3AMUniqueStatesBSF(Game g, int it) {
+			_missilesUniqueStates(g, it, 3, new BreadthFirstTreeSearch());
+		}
+	}
+}
---
+++ Benchmarks/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Brimstone.Benchmark
+{
+	internal class Supervisor
+	{
+		private TextWriter cOut;
+		private Stopwatch sw;
+		private StringBuilder csv = new StringBuilder();
+
+		public void Run(Test test) {
+			var game = test.SetupCode();
+			var testName = test.Name + (test.Iterations > 1 ? "; " + test.Iterations + " iterations" : "");
+			Start(testName);
+			test.BenchmarkCode(game, test.Iterations);
+			var result = Result();
+			csv.AppendLine(string.Format("{0},{1}", testName, sw.ElapsedMilliseconds));
+		}
+
+		public void Start(string testName) {
+			if (!string.IsNullOrEmpty(testName))
+				Console.Write(testName.PadRight(100));
+			cOut = Console.Out;
+			Console.SetOut(TextWriter.Null);
+			sw = new Stopwatch();
+			sw.Start();
+		}
+
+		public Stopwatch Result() {
+			sw.Stop();
+			Console.SetOut(cOut);
+			Console.WriteLine(sw.ElapsedMilliseconds + "ms");
+			return sw;
+		}
+
+		public void WriteResults(string path) {
+			csv.Insert(0,
+			"Build," +
+#if DEBUG
+			"Debug " +
+#else
+			"Release " +
+#endif
+			Assembly.GetAssembly(typeof(Game)).GetName().Version + "\r\n" +
+			"\"\",\"\"\r\nTest Name,Result (ms)\r\n");
+			File.WriteAllText(path, csv.ToString());
+		}
+	}
+
+	internal struct Test
+	{
+		public string Name;
+		public int Iterations;
+		public Func<Game> SetupCode;
+		public Action<Game, int> BenchmarkCode;
+
+		public Test(string ln, Action<Game, int> benchmark, Func<Game> setup = null, int it = Benchmarks.DefaultIterations) {
+			Name = ln;
+			Iterations = it;
+			SetupCode = setup ?? Benchmarks.Default_Setup;
+			BenchmarkCode = benchmark;
+		}
+	}
+
+	internal partial class Benchmarks
+	{
+		public Dictionary<string, Test> Tests;
+
+		// Create and start a game with Player 1 as the first player and no decks
+		public static Game NewEmptyGame() {
+			var game = new Game(HeroClass.Druid, HeroClass.Druid, PowerHistory: true);
+			Debug.Assert(game.Entities.Count == 6);
+			game.Start(1);
+			return game;
+		}
+
+		// Create and start a game with Player 1 as the first player with randomly filled decks
+		public static Game NewPopulatedGame() {
+			var cOut = Console.Out;
+			Console.SetOut(TextWriter.Null);
+			var game = new Game(HeroClass.Druid, HeroClass.Druid, PowerHistory: true);
+			game.Player1.Deck.Fill();
+			game.Player2.Deck.Fill();
+			Debug.Assert(game.Entities.Count == 66);
+			game.Start(1);
+			Console.SetOut(cOut);
+			return game;
+		}
+
+		// Create and start a game with Player 1 as the first player,
+		// using MaxMinions per side of the board, of which NumBoomBots are Boom Bots
+		// and the rest are the minion specified by the card name in FillMinion
+		public static Game NewScenarioGame(int MaxMinions, int NumBoomBots, string FillMinion, bool FillDeck = true) {
+			var cOut = Console.Out;
+			Console.SetOut(TextWriter.Null);
+			var game = new Game(HeroClass.Druid, HeroClass.Druid, PowerHistory: true);
+			if (FillDeck) {
+				game.Player1.Deck.Fill();
+				game.Player2.Deck.Fill();
+			}
+			game.Start(1);
+
+			for (int i = 0; i < MaxMinions - NumBoomBots; i++)
+				game.CurrentPlayer.Give(FillMinion).Play();
+			for (int i = 0; i < NumBoomBots; i++)
+				game.CurrentPlayer.Give("Boom Bot").Play();
+			game.BeginTurn();
+			for (int i = 0; i < MaxMinions - NumBoomBots; i++)
+				game.CurrentPlayer.Give(FillMinion).Play();
+			for (int i = 0; i < NumBoomBots; i++)
+				game.CurrentPlayer.Give("Boom Bot").Play();
+			Console.SetOut(cOut);
+			return game;
+		}
+
+		public void Run(string filter) {
+			var benchmark = new Supervisor();
+
+			foreach (var kv in Tests)
+				if (kv.Key.ToLower().Contains(filter)) {
+					Console.Write(("Test [" + kv.Key + "]: ").PadRight(40));
+					benchmark.Run(kv.Value);
+				}
+
+			var path = "benchmarks.csv";
+			benchmark.WriteResults(path);
+			Console.WriteLine("Benchmark results written to: " + path);
+		}
+
+		static void Main(string[] args) {
+			string filter = "";
+
+			foreach (string arg in args) {
+				try {
+					string name = arg.Substring(0, arg.IndexOf("=")).ToLower().Trim();
+					string value = arg.Substring(name.Length + 1);
+					switch (name) {
+						case "--filter":
+							filter += value.ToLower();
+							break;
+						default:
+							Console.WriteLine("Usage: benchmarks [--filter=text]...");
+							return;
+					}
+				} catch (Exception) {
+					Console.WriteLine("Usage: benchmarks [--filter=text]...");
+					return;
+				}
+			}
+
+			Console.WriteLine("Benchmarks for Brimstone build " + Assembly.GetAssembly(typeof(Game)).GetName().Version);
+#if DEBUG
+			Console.WriteLine("WARNING: Running in Debug mode. Benchmarks will perform worse than Release builds.");
+#endif
+			if (!string.IsNullOrEmpty(filter))
+				Console.WriteLine("Running benchmarks using filter: " + filter);
+
+			var b = new Benchmarks();
+			b.LoadDefinitions();
+			b.Run(filter);
+		}
+	}
+}
