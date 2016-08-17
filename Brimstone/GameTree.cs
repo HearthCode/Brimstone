91a7fd2e9454732109be36d6808e5badead1fa2b
Katy Coe <djkaty@start.no>
2016-08-17 18:27:05 +0200

    Parallelize BFS, GameTree code clean-up and comments

--- Brimstone/GameTree.cs
+++ Brimstone/GameTree.cs
@@ -1,17 +1,26 @@
 ﻿//#define _TREE_DEBUG
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Brimstone
 {
 	public interface ITreeSearcher
 	{
+		// The tree which owns this search object
+		GameTree Tree { get; set; }
+		// (Optional) code to execute after each node's ActionQueue is processed or cancelled
 		void Visitor(Game cloned, GameTree tree, QueueActionEventArgs e);
+		// (Optional) code to execute after each non-cancelled action in a node's ActionQueue completes
 		void PostAction(ActionQueue q, GameTree tree, QueueActionEventArgs e);
+		// (Optional) code to execute after all nodes have been processed
+		// NOTE: This can start a new round of processing if desired (eg. BFS)
 		Task PostProcess(GameTree tree);
+		// Return all of the unique game states (leaf nodes) found in the search results
 		Dictionary<Game, double> GetUniqueGames();
 	}
 
@@ -51,6 +60,7 @@ namespace Brimstone
 	{
 		public GameNode RootNode { get; }
 		public bool TrackChildren { get; set; }
+		public bool Parallel { get; }
 
 		// The total number of clones including the root node in this tree
 		private volatile int _nodeCount = 0;
@@ -71,14 +81,15 @@ namespace Brimstone
 		}
 
 		public GameTree(Game Root, ITreeSearcher SearchMode = null, bool CloneRoot = false, bool? Parallel = null) {
+			this.Parallel = Parallel ?? Settings.ParallelTreeSearch;
+
 			var rootGame = CloneRoot ? Root.CloneState() as Game : Root;
-			var parallel = Parallel ?? Settings.ParallelTreeSearch;
 
 			TrackChildren = (SearchMode == null);
 			RootNode = new GameNode(Game: rootGame, TrackChildren: TrackChildren);
 
 			if (SearchMode != null) {
-				if (parallel) {
+				if (this.Parallel) {
 					RootNode.Game.ActionQueue.ReplaceAction<RandomChoice>(replaceRandomChoiceParallel);
 					RootNode.Game.ActionQueue.ReplaceAction<RandomAmount>(replaceRandomAmountParallel);
 				} else {
@@ -94,11 +105,13 @@ namespace Brimstone
 		}
 
 		public void Run(Action Action) {
+			searcher.Tree = this;
 			Action();
 			searcher.PostProcess(this).Wait();
 		}
 
 		public async Task RunAsync(Action Action) {
+			searcher.Tree = this;
 			Action();
 			await searcher.PostProcess(this);
 		}
@@ -123,8 +136,8 @@ namespace Brimstone
 			// Choosing a random entity (minion in this case)
 			// Clone and start processing for every possibility
 #if _TREE_DEBUG
-			Console.WriteLine("");
-			Console.WriteLine("--> Depth: " + e.Game.Depth);
+			DebugLog.WriteLine("");
+			DebugLog.WriteLine("--> Depth: " + e.Game.Depth);
 #endif
 			double perItemWeight = 1.0 / e.Args[RandomChoice.ENTITIES].Count();
 			foreach (Entity entity in e.Args[RandomChoice.ENTITIES]) {
@@ -138,8 +151,8 @@ namespace Brimstone
 				searcher.Visitor(cloned, this, e);
 			}
 #if _TREE_DEBUG
-			Console.WriteLine("<-- Depth: " + e.Game.Depth);
-			Console.WriteLine("");
+			DebugLog.WriteLine("<-- Depth: " + e.Game.Depth);
+			DebugLog.WriteLine("");
 #endif
 			return Task.FromResult(0);
 		}
@@ -148,8 +161,8 @@ namespace Brimstone
 			// Choosing a random value (damage amount in this case)
 			// Clone and start processing for every possibility
 #if _TREE_DEBUG
-			Console.WriteLine("");
-			Console.WriteLine("--> Depth: " + e.Game.Depth);
+			DebugLog.WriteLine("");
+			DebugLog.WriteLine("--> Depth: " + e.Game.Depth);
 #endif
 			double perItemWeight = 1.0 / ((e.Args[RandomAmount.MAX] - e.Args[RandomAmount.MIN]) + 1);
 			for (int i = e.Args[RandomAmount.MIN]; i <= e.Args[RandomAmount.MAX]; i++) {
@@ -163,8 +176,8 @@ namespace Brimstone
 				searcher.Visitor(cloned, this, e);
 			}
 #if _TREE_DEBUG
-			Console.WriteLine("<-- Depth: " + e.Game.Depth);
-			Console.WriteLine("");
+			DebugLog.WriteLine("<-- Depth: " + e.Game.Depth);
+			DebugLog.WriteLine("");
 #endif
 			return Task.FromResult(0);
 		}
@@ -173,8 +186,8 @@ namespace Brimstone
 			// Choosing a random entity (minion in this case)
 			// Clone and start processing for every possibility
 #if _TREE_DEBUG
-			Console.WriteLine("");
-			Console.WriteLine("--> Depth: " + e.Game.Depth);
+			DebugLog.WriteLine("");
+			DebugLog.WriteLine("--> Depth: " + e.Game.Depth);
 #endif
 			double perItemWeight = 1.0 / e.Args[RandomChoice.ENTITIES].Count();
 			await Task.WhenAll(
@@ -192,8 +205,8 @@ namespace Brimstone
 				)
 			);
 #if _TREE_DEBUG
-			Console.WriteLine("<-- Depth: " + e.Game.Depth);
-			Console.WriteLine("");
+			DebugLog.WriteLine("<-- Depth: " + e.Game.Depth);
+			DebugLog.WriteLine("");
 #endif
 		}
 
@@ -201,8 +214,8 @@ namespace Brimstone
 			// Choosing a random value (damage amount in this case)
 			// Clone and start processing for every possibility
 #if _TREE_DEBUG
-			Console.WriteLine("");
-			Console.WriteLine("--> Depth: " + e.Game.Depth);
+			DebugLog.WriteLine("");
+			DebugLog.WriteLine("--> Depth: " + e.Game.Depth);
 #endif
 			double perItemWeight = 1.0 / ((e.Args[RandomAmount.MAX] - e.Args[RandomAmount.MIN]) + 1);
 			await Task.WhenAll(
@@ -220,18 +233,29 @@ namespace Brimstone
 				)
 			);
 #if _TREE_DEBUG
-			Console.WriteLine("<-- Depth: " + e.Game.Depth);
-			Console.WriteLine("");
+			DebugLog.WriteLine("<-- Depth: " + e.Game.Depth);
+			DebugLog.WriteLine("");
 #endif
 		}
 	}
 
-	public class NaiveTreeSearch : ITreeSearcher
+	public abstract class TreeSearch : ITreeSearcher
+	{
+		public GameTree Tree { get; set; }
+
+		public abstract Dictionary<Game, double> GetUniqueGames();
+		public virtual void PostAction(ActionQueue q, GameTree tree, QueueActionEventArgs e) { }
+		public virtual Task PostProcess(GameTree tree) { return Task.FromResult(0); }
+		public virtual void Visitor(Game cloned, GameTree tree, QueueActionEventArgs e) { }
+	}
+
+	public class NaiveTreeSearch : TreeSearch
 	{
 		private HashSet<Game> leafNodeGames = new HashSet<Game>();
 
-		public void Visitor(Game cloned, GameTree tree, QueueActionEventArgs e) {
+		public override void Visitor(Game cloned, GameTree tree, QueueActionEventArgs e) {
 			// If the action queue is empty, we have reached a leaf node game state
+			// TODO: Optimize to use TLS and avoid spinlocks
 			if (cloned.ActionQueue.Queue.Count == 0) {
 				tree.LeafNodeCount++;
 				lock (leafNodeGames) {
@@ -240,13 +264,10 @@ namespace Brimstone
 			}
 		}
 
-		public void PostAction(ActionQueue q, GameTree t, QueueActionEventArgs e) {	}
-
-		public Task PostProcess(GameTree t) { return Task.FromResult(0); }
-
-		public Dictionary<Game, double> GetUniqueGames() {
+		public override Dictionary<Game, double> GetUniqueGames() {
 			var uniqueGames = new Dictionary<Game, double>();
 
+			// TODO: Parallelize
 			while (leafNodeGames.Count > 0) {
 				var root = leafNodeGames.Take(1).ToList()[0];
 				leafNodeGames.Remove(root);
@@ -284,9 +305,9 @@ namespace Brimstone
 									}
 								}
 								if (collide) {
-									Console.WriteLine(collisions[e.FuzzyHash]);
-									Console.WriteLine(e);
-									Console.WriteLine(collisions[e.FuzzyHash].FuzzyHash + " " + e.FuzzyHash);
+									DebugLog.WriteLine(collisions[e.FuzzyHash].ToString());
+									DebugLog.WriteLine(e.ToString());
+									DebugLog.WriteLine(collisions[e.FuzzyHash].FuzzyHash + " " + e.FuzzyHash);
 									throw new Exception("Hash collision - not safe to compare games");
 								}
 							}
@@ -303,130 +324,197 @@ namespace Brimstone
 				}
 				leafNodeGames = different;
 #if _TREE_DEBUG
-				Console.WriteLine("{0} games remaining to process ({1} unique games found so far)", different.Count, uniqueGames.Count);
+				DebugLog.WriteLine("{0} games remaining to process ({1} unique games found so far)", different.Count, uniqueGames.Count);
 #endif
 			}
 			return uniqueGames;
 		}
 	}
 	
-	public class DepthFirstTreeSearch : ITreeSearcher
+	public class DepthFirstTreeSearch : TreeSearch
 	{
 		private Dictionary<Game, double> uniqueGames = new Dictionary<Game, double>(new FuzzyGameComparer());
 
-		public void Visitor(Game cloned, GameTree tree, QueueActionEventArgs e) {
+		public override void Visitor(Game cloned, GameTree tree, QueueActionEventArgs e) {
 			// If the action queue is empty, we have reached a leaf node game state
 			// so compare it for equality with other final game states
 			if (cloned.ActionQueue.Queue.Count == 0)
 				if (!cloned.EquivalentTo(e.Game)) {
 					tree.LeafNodeCount++;
 					// This will cause the game to be discarded if its fuzzy hash matches any other final game state
+					// TODO: Optimize to use TLS and avoid spinlocks
 					lock (uniqueGames) {
 						if (!uniqueGames.ContainsKey(cloned)) {
 							uniqueGames.Add(cloned, ((GameNode)cloned.CustomData).Probability);
 #if _TREE_DEBUG
-							Console.WriteLine("UNIQUE GAME FOUND ({0})", uniqueGames.Count);
+							DebugLog.WriteLine("UNIQUE GAME FOUND ({0})", uniqueGames.Count);
 #endif
 						}
 						else {
 							uniqueGames[cloned] += ((GameNode)cloned.CustomData).Probability;
 #if _TREE_DEBUG
-							Console.WriteLine("DUPLICATE GAME FOUND");
+							DebugLog.WriteLine("DUPLICATE GAME FOUND");
 #endif
 						}
 					}
 				}
 		}
 
-		public void PostAction(ActionQueue q, GameTree t, QueueActionEventArgs e) { }
-
-		public Task PostProcess(GameTree t) { return Task.FromResult(0); }
-
-		public Dictionary<Game, double> GetUniqueGames() {
+		public override Dictionary<Game, double> GetUniqueGames() {
 			return uniqueGames;
 		}
 	}
 
-	public class BreadthFirstTreeSearch : ITreeSearcher
+	public class BreadthFirstTreeSearch : TreeSearch
 	{
+		// The maximum number of task threads to split the search queue up into
+		public int MaxDegreesOfParallelism { get; set; } = 5;
+
+		// The minimum number of game nodes that have to be in the queue in order to activate parallelization
+		// for a particular depth
+		public int MinNodesToParallelize { get; set; } = 100;
+
+		// All of the unique leaf node games found
 		private Dictionary<Game, double> uniqueGames = new Dictionary<Game, double>(new FuzzyGameComparer());
 
 		// The pruned search queue for the current search depth
 		private Dictionary<Game, double> searchQueue = new Dictionary<Game, double>(new FuzzyGameComparer());
 
-		public void Visitor(Game cloned, GameTree tree, QueueActionEventArgs e) { }
+		// Per-thread storage for partitioned search queue and unique games found
+		// (merged with the main lists after each depth is searched)
+		private ThreadLocal<Dictionary<Game, double>> tlsSearchQueue = new ThreadLocal<Dictionary<Game, double>>(() => new Dictionary<Game, double>(new FuzzyGameComparer()), trackAllValues: true);
+		private ThreadLocal<Dictionary<Game, double>> tlsUniqueGames = new ThreadLocal<Dictionary<Game, double>>(() => new Dictionary<Game, double>(new FuzzyGameComparer()), trackAllValues: true);
 
 		// When an in-game action completes, check if the game state has changed
 		// Some actions (like selectors) won't cause the game state to change,
 		// so we continue running these until a game state change occurs
-		public void PostAction(ActionQueue q, GameTree t, QueueActionEventArgs e) {
+		public override void PostAction(ActionQueue q, GameTree t, QueueActionEventArgs e) {
+			// This game will be on the same thread as the calling task in parallel mode if it hasn't been cloned
+			// If it has been cloned, it may be on a different thread
 			if (e.Game.Entities.Changed) {
 				// If the action queue is empty, we have reached a leaf node game state
 				// so compare it for equality with other final game states
 				if (e.Game.ActionQueue.Queue.Count == 0) {
 					t.LeafNodeCount++;
 					// This will cause the game to be discarded if its fuzzy hash matches any other final game state
-					lock (uniqueGames) {
-						if (!uniqueGames.ContainsKey(e.Game)) {
-							uniqueGames.Add(e.Game, ((GameNode)e.Game.CustomData).Probability);
+					if (!tlsUniqueGames.Value.ContainsKey(e.Game)) {
+						tlsUniqueGames.Value.Add(e.Game, ((GameNode)e.Game.CustomData).Probability);
 #if _TREE_DEBUG
-						Console.WriteLine("UNIQUE GAME FOUND ({0})", uniqueGames.Count);
+						DebugLog.WriteLine("UNIQUE GAME FOUND ({0})", uniqueGames.Count);
 #endif
-						}
-						else {
-							uniqueGames[e.Game] += ((GameNode)e.Game.CustomData).Probability;
+					}
+					else {
+						tlsUniqueGames.Value[e.Game] += ((GameNode)e.Game.CustomData).Probability;
 #if _TREE_DEBUG
-						Console.WriteLine("DUPLICATE GAME FOUND");
+						DebugLog.WriteLine("DUPLICATE GAME FOUND");
 #endif
-						}
 					}
 				}
 				else {
 					// The game state has changed but there are more actions to do
 					// (which may or may not involve further cloning) so add it to the search queue
 #if _TREE_DEBUG
-					Console.WriteLine("QUEUEING FOR NEXT SEARCH");
+					DebugLog.WriteLine("QUEUEING FOR NEXT SEARCH");
 #endif
-					lock (searchQueue) {
-						if (!searchQueue.ContainsKey(e.Game))
-							searchQueue.Add(e.Game, ((GameNode)e.Game.CustomData).Probability);
-						else
-							searchQueue[e.Game] += ((GameNode)e.Game.CustomData).Probability;
-					}
+					if (!tlsSearchQueue.Value.ContainsKey(e.Game))
+						tlsSearchQueue.Value.Add(e.Game, ((GameNode)e.Game.CustomData).Probability);
+					else
+						tlsSearchQueue.Value[e.Game] += ((GameNode)e.Game.CustomData).Probability;
 				}
 #if _TREE_DEBUG
-				Console.WriteLine("");
+				DebugLog.WriteLine("");
 #endif
 				e.Cancel = true;
 			}
 		}
 
-		public Task PostProcess(GameTree t) {
+		// This is the entry point after the root node has been pushed into the queue
+		// and the first change to the game has occurred
+		public override async Task PostProcess(GameTree t) {
 			// Breadth-first processing loop
-			while (searchQueue.Count > 0) {
+			do {
+				// Merge the TLS lists into the main lists
+				foreach (var sq in tlsSearchQueue.Values) {
+					foreach (var qi in sq) {
+						if (!searchQueue.ContainsKey(qi.Key))
+							searchQueue.Add(qi.Key, qi.Value);
+						else
+							searchQueue[qi.Key] += qi.Value;
+					}
+				}
+				foreach (var ug in tlsUniqueGames.Values) {
+					foreach (var qi in ug) {
+						if (!uniqueGames.ContainsKey(qi.Key))
+							uniqueGames.Add(qi.Key, qi.Value);
+						else
+							uniqueGames[qi.Key] += qi.Value;
+					}
+				}
 #if _TREE_DEBUG
-				Console.WriteLine("QUEUE SIZE: " + searchQueue.Count);
+				DebugLog.WriteLine("QUEUE SIZE: " + searchQueue.Count);
 #endif
+				// Wipe the TLS lists
+				tlsSearchQueue = new ThreadLocal<Dictionary<Game, double>>(() => new Dictionary<Game, double>(new FuzzyGameComparer()), trackAllValues: true);
+				tlsUniqueGames = new ThreadLocal<Dictionary<Game, double>>(() => new Dictionary<Game, double>(new FuzzyGameComparer()), trackAllValues: true);
+
+				// Quit if we have processed all nodes and none of them have children (all leaf nodes)
+				if (searchQueue.Count == 0)
+					break;
+
 				// Copy the search queue and clear the current one; it will be refilled
 				var nextQueue = new Dictionary<Game, double>(searchQueue);
 				searchQueue.Clear();
 
-				// Process each game's action queue until it is interrupted by OnAction above
-				foreach (var kv in nextQueue) {
-					((GameNode)kv.Key.CustomData).Probability = kv.Value;
-					kv.Key.ActionQueue.ProcessAll();
-				}
+				// Only parallelize if there are sufficient nodes to do so
+				if (nextQueue.Count >= MinNodesToParallelize && Tree.Parallel) {
+					// Process each game's action queue until it is interrupted by OnAction above
+					await Task.WhenAll(
+						// Split search queue into MaxDegreesOfParallelism partitions
+						from partition in Partitioner.Create(nextQueue).GetPartitions(MaxDegreesOfParallelism)
+						// Run each partition in its own task
+						select Task.Run(async delegate {
 #if _TREE_DEBUG
-				Console.WriteLine("=======================");
-				Console.WriteLine("CLONES SO FAR: " + t.NodeCount + " / " + t.LeafNodeCount);
-				Console.WriteLine("UNIQUE GAMES SO FAR: " + uniqueGames.Count);
-				Console.WriteLine("NEW QUEUE SIZE: " + searchQueue.Count + "\r\n");
+							var count = 0;
+							DebugLog.WriteLine("Start partition run");
 #endif
-			}
-			return Task.FromResult(0);
+							using (partition)
+								while (partition.MoveNext()) {
+									// Process each node
+									var kv = partition.Current;
+									((GameNode)kv.Key.CustomData).Probability = kv.Value;
+									await kv.Key.ActionQueue.ProcessAllAsync();
+#if _TREE_DEBUG
+									count++;
+#endif
+								}
+#if _TREE_DEBUG
+							DebugLog.WriteLine("End run with partition size {0}", count);
+#endif
+						}));
+#if _TREE_DEBUG
+				DebugLog.WriteLine("=======================");
+				DebugLog.WriteLine("CLONES SO FAR: " + t.NodeCount + " / " + t.LeafNodeCount);
+				DebugLog.WriteLine("UNIQUE GAMES SO FAR: " + uniqueGames.Count);
+				DebugLog.WriteLine("NEW QUEUE SIZE: " + searchQueue.Count + "\r\n");
+#endif
+				}
+				else {
+#if _TREE_DEBUG
+					DebugLog.WriteLine("Start single-threaded run");
+#endif
+					// Process each node in the search queue sequentially
+					foreach (var kv in nextQueue) {
+						((GameNode)kv.Key.CustomData).Probability = kv.Value;
+						await kv.Key.ActionQueue.ProcessAllAsync();
+					}
+#if _TREE_DEBUG
+					DebugLog.WriteLine("End single-threaded run");
+#endif
+				}
+			} while (true);
 		}
 
-		public Dictionary<Game, double> GetUniqueGames() {
+		public override Dictionary<Game, double> GetUniqueGames() {
 			return uniqueGames;
 		}
 	}
