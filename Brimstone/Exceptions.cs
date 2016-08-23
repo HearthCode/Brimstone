a73e823391d3335fc40da930d0db417aafbd55a5
Katy Coe <djkaty@start.no>
2016-08-23 03:16:13 +0200

    Add PendingChoiceException

---
+++ Brimstone/Exceptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Brimstone
+{
+	// We can't continue until a player has made a choice (ie. mulligan, discover)
+	public class PendingChoiceException : Exception
+	{
+		public PendingChoiceException() { }
+
+		public PendingChoiceException(string message) : base(message) { }
+
+		public PendingChoiceException(string message, Exception inner) : base(message, inner) { }
+	}
+}
