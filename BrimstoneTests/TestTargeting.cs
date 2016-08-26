5630698485a660ad7704763cd8314d7f5fed773e
Katy Coe <djkaty@start.no>
2016-08-26 05:33:27 +0200

    Clean up TestTargeting

--- BrimstoneTests/TestTargeting.cs
+++ BrimstoneTests/TestTargeting.cs
@@ -10,33 +10,34 @@ namespace BrimstoneTests
 	public class TestTargeting
 	{
 		[Test]
-		public void TestFireballTargeting([Values(0,1)] int playerToAct) {
+		public void TestSpellTargeting([Values(1, 2)] int playerToAct) {
 			// Arrange
 			var game = new Game(HeroClass.Priest, HeroClass.Priest);
 			game.Start(SkipMulligan: true);
 
 			var p1 = game.CurrentPlayer;
-			var p2 = game.CurrentPlayer.Opponent;		
-			var pAct = game.Players[playerToAct];
+			var p2 = game.CurrentPlayer.Opponent;
+			var pAct = game.Players[playerToAct - 1];
 
 			// Add a spell and some targets
-			var spell = pAct.Give("Fireball");
+			var spell = pAct.Give("Fireball") as ICanTarget;
 
 			var in1 = p1.Give("Goldshire Footman").Play();
 			var in2 = p1.Give("Goldshire Footman").Play();
 			var in3 = p2.Give("Goldshire Footman").Play();
+			var in4 = p1.Give("Stranglethorn Tiger").Play();
 
 			var out1 = p1.Give("Faerie Dragon").Play();
 			var out2 = p2.Give("Faerie Dragon").Play();
+			var out3 = p2.Give("Stranglethorn Tiger").Play();
 
 			// Act
-			bool requiresTarget = spell.Card.RequiresTarget;
-			List<IEntity> targets = ((ICanTarget)spell).ValidTargets;
+			var targets = spell.ValidTargets;
 
 			// Assert
 
 			// Fireball must require a target
-			Assert.AreEqual(true, requiresTarget);
+			Assert.True(spell.Card.RequiresTarget);
 
 			// All heroes must be valid targets
 			CollectionAssert.Contains(targets, p1.Hero);
@@ -47,25 +48,31 @@ namespace BrimstoneTests
 			CollectionAssert.Contains(targets, in2);
 			CollectionAssert.Contains(targets, in3);
 
+			// Our Stanglethorn Tiger must be a valid target
+			CollectionAssert.Contains(targets, in4);
+
 			// No Faerie Dragon must be a valid target
 			CollectionAssert.DoesNotContain(targets, out1);
 			CollectionAssert.DoesNotContain(targets, out2);
 
+			// Opponent Stranglethorn Tiger must not be a valid target
+			CollectionAssert.DoesNotContain(targets, out3);
+
 			// No other entities must be in the valid targets list
-			Assert.AreEqual(targets.Count, 5);
+			Assert.AreEqual(targets.Count, 6);
 		}
 
 		[Test]
-		public void TestHoundmasterTargeting() {
+		public void TestFriendlyTargetWithRaceTargeting() {
 			// Arrange
 			var game = new Game(HeroClass.Priest, HeroClass.Priest);
 			game.Start(SkipMulligan: true);
 
 			var p1 = game.CurrentPlayer;
-			var p2 = game.CurrentPlayer.Opponent;		
+			var p2 = game.CurrentPlayer.Opponent;
 
 			// Add Houndmaster to hands and add some targets
-			var houndmaster = p1.Give("Houndmaster");
+			var houndmaster = p1.Give("Houndmaster") as ICanTarget;
 
 			var in1 = p1.Give("Bloodfen Raptor").Play();
 			var in2 = p1.Give("Bloodfen Raptor").Play();
@@ -75,13 +82,15 @@ namespace BrimstoneTests
 			var out3 = p2.Give("Bloodfen Raptor").Play();
 
 			// Act
-			bool requiresTarget = houndmaster.Card.RequiresTarget;
-			List<IEntity> targets = ((ICanTarget)houndmaster).ValidTargets;
+			var targets = houndmaster.ValidTargets;
 
 			// Assert
 
 			// Houndmaster must not require a target
-			Assert.AreEqual(false, requiresTarget);
+			Assert.False(houndmaster.Card.RequiresTarget);
+
+			// Must require a target if one is available
+			Assert.True(houndmaster.Card.RequiresTargetIfAvailable);
 
 			// All friendly Bloodfen Raptors must be valid targets
 			CollectionAssert.Contains(targets, in1);
@@ -97,173 +106,71 @@ namespace BrimstoneTests
 		}
 
 		[Test]
-		public void TestGormokActiveTargeting() {
+		public void TestMinimumMinionsTargeting() {
 			// Arrange
 			var game = new Game(HeroClass.Priest, HeroClass.Priest);
 			game.Start(SkipMulligan: true);
 
 			var p1 = game.CurrentPlayer;
-			var p2 = game.CurrentPlayer.Opponent;		
+			var p2 = game.CurrentPlayer.Opponent;
 
 			// Add Gormok to hand and add some targets
-			var gormok = p1.Give("Gormok the Impaler");
+			var gormok = p1.Give("Gormok the Impaler") as ICanTarget;
 
 			for (int i = 0; i < 4; i++)
 				p1.Give("Wisp").Play();
 			for (int i = 0; i < 6; i++)
 				p2.Give("Wisp").Play();
-
-			// Act
-			bool requiresTarget = gormok.Card.RequiresTarget;
-			List<IEntity> targets = ((ICanTarget)gormok).ValidTargets;
 
 			// Assert
 
 			// Gormok must not require a target
-			Assert.AreEqual(false, requiresTarget);
+			Assert.False(gormok.Card.RequiresTarget);
 
 			// The valid targets must be exactly the whole board and heroes
-			var validTargets = p1.Board.Concat(p2.Board).Concat(new List<IEntity> { p1.Hero, p2.Hero });
-			CollectionAssert.AreEquivalent(validTargets, targets);
+			CollectionAssert.AreEquivalent(game.Characters, gormok.ValidTargets);
+
+			// Remove a minion
+			p1.Board[4].Zone = p1.Graveyard;
+
+			// Test again
+			Assert.AreEqual(0, gormok.ValidTargets.Count);
 		}
 
 		[Test]
-		public void TestGormokInactiveTargeting() {
+		public void TestDragonInHandTargeting() {
 			// Arrange
 			var game = new Game(HeroClass.Priest, HeroClass.Priest);
 			game.Start(SkipMulligan: true);
 
 			var p1 = game.CurrentPlayer;
-			var p2 = game.CurrentPlayer.Opponent;		
-
-			// Add Gormok to hand and add some targets
-			var gormok = p1.Give("Gormok the Impaler");
-
-			for (int i = 0; i < 3; i++)
-				p1.Give("Wisp").Play();
-			for (int i = 0; i < 6; i++)
-				p2.Give("Wisp").Play();
-
-			// Act
-			List<IEntity> targets = ((ICanTarget)gormok).ValidTargets;
-
-			// Assert
-
-			// Gormok must not have any valid targets
-			Assert.AreEqual(0, targets.Count);
-		}
-
-		[Test]
-		public void TestBlackwingActiveTargeting() {
-			// Arrange
-			var game = new Game(HeroClass.Priest, HeroClass.Priest);
-			game.Start(SkipMulligan: true);
-
-			var p1 = game.CurrentPlayer;
-			var p2 = game.CurrentPlayer.Opponent;		
+			var p2 = game.CurrentPlayer.Opponent;
 
 			// Add Blackwing to hand and add some targets
-			var blackwing = p1.Give("Blackwing Corruptor");
+			var blackwing = p1.Give("Blackwing Corruptor") as ICanTarget;
 
 			for (int i = 0; i < 4; i++)
 				p1.Give("Wisp").Play();
 			for (int i = 0; i < 6; i++)
 				p2.Give("Wisp").Play();
-
-			// Activate Blackwing
-			p1.Give("Azure Drake");
-
-			// Act
-			bool requiresTarget = blackwing.Card.RequiresTarget;
-			List<IEntity> targets = ((ICanTarget)blackwing).ValidTargets;
 
 			// Assert
 
 			// Blackwing must not require a target
-			Assert.AreEqual(false, requiresTarget);
+			Assert.False(blackwing.Card.RequiresTarget);
+
+			// No targets right now because requirements not met
+			Assert.AreEqual(0, blackwing.ValidTargets.Count);
+
+			// Act
+
+			// Activate Blackwing (fulfill targeting requirements)
+			p1.Give("Azure Drake");
+
+			// Assert
 
 			// The valid targets must be exactly the whole board and heroes
-			var validTargets = p1.Board.Concat(p2.Board).Concat(new List<IEntity> { p1.Hero, p2.Hero });
-			CollectionAssert.AreEquivalent(validTargets, targets);
-		}
-
-		[Test]
-		public void TestBlackwingInactiveTargeting() {
-			// Arrange
-			var game = new Game(HeroClass.Priest, HeroClass.Priest);
-			game.Start(SkipMulligan: true);
-
-			var p1 = game.CurrentPlayer;
-			var p2 = game.CurrentPlayer.Opponent;		
-
-			// Add Gormok to hand and add some targets
-			var blackwing = p1.Give("Blackwing Corruptor");
-
-			for (int i = 0; i < 4; i++)
-				p1.Give("Wisp").Play();
-			for (int i = 0; i < 6; i++)
-				p2.Give("Wisp").Play();
-
-			// Act
-			List<IEntity> targets = ((ICanTarget)blackwing).ValidTargets;
-
-			// Assert
-
-			// Blackwing must not have any valid targets
-			Assert.AreEqual(0, targets.Count);
-		}
-
-
-		[Test]
-		public void TestAttackTargetingWithTaunt() {
-			// Arrange
-			var game = new Game(HeroClass.Priest, HeroClass.Priest);
-			game.Start(SkipMulligan: true);
-
-			var p1 = game.CurrentPlayer;
-			var p2 = game.CurrentPlayer.Opponent;
-
-			// Add friendly minions
-			for (int i = 0; i < 3; i++)
-				p1.Give("Wisp").Play();
-			var attacker = p1.Give("Worgen Infiltrator").Play();
-			p1.Give("Worgen Infiltrator").Play();
-
-			// Add enemy minions
-			var validTargets = new List<IEntity>();
-			for (int i = 0; i < 3; i++)
-				validTargets.Add(p2.Give("Wisp").Play());
-			p2.Give("Worgen Infiltrator").Play();
-			validTargets.Add(p2.Hero);
-
-			// Act
-			List<IEntity> targets = ((ICanTarget)attacker).ValidTargets;
-
-			// Assert
-
-			// Must be able to attack all opponent characters
-			CollectionAssert.AreEquivalent(validTargets, targets);
-		}
-
-		[Test]
-		public void TestAttackTargetingOnlyHero() {
-			// Arrange
-			var game = new Game(HeroClass.Priest, HeroClass.Priest);
-			game.Start(SkipMulligan: true);
-
-			var p1 = game.CurrentPlayer;
-			var p2 = game.CurrentPlayer.Opponent;
-
-			// Add friendly minions
-			var attacker = p1.Give("Raging Worgen").Play();
-
-			// Act
-			List<IEntity> targets = ((ICanTarget)attacker).ValidTargets;
-
-			// Assert
-
-			// Must be able to attack only enemy hero
-			CollectionAssert.AreEquivalent(new List<IEntity> { p2.Hero }, targets);
+			CollectionAssert.AreEquivalent(game.Characters, blackwing.ValidTargets);
 		}
 
 		[Test]
@@ -278,7 +185,53 @@ namespace BrimstoneTests
 			// Add friendly minions
 			for (int i = 0; i < 3; i++)
 				p1.Give("Wisp").Play();
-			var attacker = p1.Give("Worgen Infiltrator").Play();
+			var attacker = p1.Give("Worgen Infiltrator").Play() as ICanTarget;
+			p1.Give("Worgen Infiltrator").Play();
+
+			// Add enemy minions
+			var validTargets = new List<IEntity>();
+			for (int i = 0; i < 3; i++)
+				validTargets.Add(p2.Give("Wisp").Play());
+			p2.Give("Worgen Infiltrator").Play();
+			validTargets.Add(p2.Hero);
+
+			// Assert
+
+			// Must be able to attack all opponent characters
+			CollectionAssert.AreEquivalent(validTargets, attacker.ValidTargets);
+		}
+
+		[Test]
+		public void TestAttackTargetingOnlyHero() {
+			// Arrange
+			var game = new Game(HeroClass.Priest, HeroClass.Priest);
+			game.Start(SkipMulligan: true);
+
+			var p1 = game.CurrentPlayer;
+			var p2 = game.CurrentPlayer.Opponent;
+
+			// Add friendly minions
+			var attacker = p1.Give("Raging Worgen").Play() as ICanTarget;
+
+			// Assert
+
+			// Must be able to attack only enemy hero
+			CollectionAssert.AreEquivalent(new List<IEntity> { p2.Hero }, attacker.ValidTargets);
+		}
+
+		[Test]
+		public void TestAttackTargetingWithTaunt() {
+			// Arrange
+			var game = new Game(HeroClass.Priest, HeroClass.Priest);
+			game.Start(SkipMulligan: true);
+
+			var p1 = game.CurrentPlayer;
+			var p2 = game.CurrentPlayer.Opponent;
+
+			// Add friendly minions
+			for (int i = 0; i < 3; i++)
+				p1.Give("Wisp").Play();
+			var attacker = p1.Give("Worgen Infiltrator").Play() as ICanTarget;
 
 			// Add enemy minions, including two taunts and one stealthy taunt
 			var validTargets = new List<IEntity>();
@@ -293,13 +246,10 @@ namespace BrimstoneTests
 			var stealthTaunt = (Minion)p2.Give("Deathlord").Play();
 			stealthTaunt.HasStealth = true;
 
-			// Act
-			List<IEntity> targets = ((ICanTarget)attacker).ValidTargets;
-
 			// Assert
 
 			// Must be able to attack all non-stealthy opponent taunts
-			CollectionAssert.AreEquivalent(validTargets, targets);
+			CollectionAssert.AreEquivalent(validTargets, attacker.ValidTargets);
 		}
 	}
 }
