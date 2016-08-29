a1d88e4f85073b3a0f212fdc29447d00b4f395d4
Katy Coe <djkaty@start.no>
2016-08-30 01:43:53 +0200

    Add Player.SufficientMana property

--- Brimstone/EntityTypes/Player.cs
+++ Brimstone/EntityTypes/Player.cs
@@ -53,6 +53,8 @@ namespace Brimstone
 
 		public int RemainingMana => (BaseMana + TemporaryMana) - (UsedMana + Overload);
 
+		public bool SufficientMana(IEntity e) => RemainingMana >= e.Cost;
+
 		// All the entities that we can potentially play or attack with when it's our turn
 		public IEnumerable<IEntity> LiveEntities => Hand.Concat(Board).Concat(new List<IEntity> {this, /* HeroPower */});
 
