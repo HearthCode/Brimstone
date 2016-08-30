2815ef2260cb92e09815260d548363d5c0fe67a1
Katy Coe <djkaty@start.no>
2016-08-30 22:17:08 +0200

    Add initial Environment with LastDamaged property

---
+++ Brimstone/Environment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brimstone
+{
+	public class Environment : ICloneable
+	{
+		public Game Game { get; set; }
+
+		public Environment(Game game) {
+			Game = game;
+		}
+
+		private int _lastDamaged;
+		public ICharacter LastDamaged {
+			get { return (ICharacter) Game.Entities[_lastDamaged]; }
+			set { _lastDamaged = value.Id; }
+		}
+
+		public object Clone() {
+			return MemberwiseClone();
+		}
+	}
+}
