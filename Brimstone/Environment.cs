9c31c72866dd8b9b5d6f394477bb0e03e7178187
Timothy Stiles <Patashu@hotmail.com>
2016-09-02 09:58:33 +1000

    Environment.cs - add LastCardPlayed, LastCardDiscarded

--- Brimstone/Environment.cs
+++ Brimstone/Environment.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,6 +18,18 @@ namespace Brimstone
 		public ICharacter LastDamaged {
 			get { return (ICharacter) Game.Entities[_lastDamaged]; }
 			set { _lastDamaged = value.Id; }
+		}
+
+		private int _lastCardPlayed;
+		public IEntity LastCardPlayed {
+			get { return (IEntity)Game.Entities[_lastCardPlayed]; }
+			set { _lastCardPlayed = value.Id; }
+		}
+
+		private int _lastCardDiscarded;
+		public IEntity LastCardDiscarded {
+			get { return (IEntity)Game.Entities[_lastCardDiscarded]; }
+			set { _lastCardDiscarded = value.Id; }
 		}
 
 		public object Clone() {
