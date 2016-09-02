460ed274f22ff0c3b2ecd58a0c8eff6ae320ef7c
Katy Coe <djkaty@start.no>
2016-09-02 21:00:25 +0200

    Don't crash or assign position on Player zone move

--- Brimstone/Zone.cs
+++ Brimstone/Zone.cs
@@ -180,7 +180,7 @@ namespace Brimstone
 						ZonePosition = 0;
 					else
 						ZonePosition = Count + 1;
-			if (Type == Zone.SETASIDE || Type == Zone.GRAVEYARD || (Type == Zone.PLAY && Controller is Game))
+			if (Type == Zone.SETASIDE || Type == Zone.GRAVEYARD || (Type == Zone.PLAY && Controller is Game) || Entity is Player)
 				ZonePosition = 0;
 			if (ZonePosition != 0) {
 				if (Entity is T)
