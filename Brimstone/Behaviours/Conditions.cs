0917e588d17adb1daf30cc5af8e9eaa1b37200c3
Timothy Stiles <Patashu@hotmail.com>
2016-09-07 14:47:56 +1000

    Conditions.cs - add IsFriendlyMinion
    (even though it's not being used yet, but hey)

--- Brimstone/Behaviours/Conditions.cs
+++ Brimstone/Behaviours/Conditions.cs
@@ -1,4 +1,4 @@
-﻿namespace Brimstone
+namespace Brimstone
 {
 	// All of the trigger conditions you can use
 	public partial class Behaviours
@@ -6,6 +6,7 @@
 		public static Condition IsSelf = new Condition((me, other) => me == other);
 		public static Condition IsFriendlySpell = new Condition((me, other) => me.Controller == other.Controller && other is Spell);
 		public static Condition IsFriendly = new Condition((me, other) => me.Controller == other.Controller);
+		public static Condition IsFriendlyMinion = new Condition((me, other) => me.Controller == other.Controller && other is Minion);
 		public static Condition IsOpposing = new Condition((me, other) => me.Controller == other.Controller.Opponent);
 	}
 }
