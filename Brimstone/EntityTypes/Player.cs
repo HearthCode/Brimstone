8c2d9e4fbc9f8597c9cf613ded1c393c6dd66f99
Katy Coe <djkaty@start.no>
2016-09-13 21:59:54 +0200

    Add Hero.Power to Player.LiveEntities

--- Brimstone/EntityTypes/Player.cs
+++ Brimstone/EntityTypes/Player.cs
@@ -55,7 +55,7 @@ namespace Brimstone
 		public bool SufficientResources(IEntity e) => RemainingMana >= e.Cost;
 
 		// All the entities that we can potentially play or attack with when it's our turn
-		public IEnumerable<IEntity> LiveEntities => Hand.Concat(Board).Concat(new List<IEntity> {this, /* HeroPower */});
+		public IEnumerable<IEntity> LiveEntities => Hand.Concat(Board).Concat(new List<IEntity> {Hero, Hero.Power});
 
 		// TODO: Cache options
 		public IEnumerable<Option> Options
