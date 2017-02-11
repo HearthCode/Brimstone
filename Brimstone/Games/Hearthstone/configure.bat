0df0eb5663e35d05b041ab2680f7ca81a360fedb
Katy Coe <djkaty@start.no>
2017-02-11 18:47:57 +0100

    Add Cards.xml acquisition scripts

--- Brimstone/Games/Hearthstone/configure.bat
+++ Brimstone/Games/Hearthstone/configure.bat
@@ -1,11 +1,14 @@
 @echo off
 set PROJECTDIR=%~1
-if not exist "%PROJECTDIR%"Games\Hearthstone\Data\CardDefs.xml (
+if not exist "%PROJECTDIR%"Games\Hearthstone\Data\Cards.xml (
 	if not exist "%PROJECTDIR%"Games\Hearthstone\Data (
 		mkdir "%PROJECTDIR%"Games\Hearthstone\Data
 	)
-	git clone https://github.com/HearthSim/hs-data
-	copy /y hs-data\CardDefs.xml "%PROJECTDIR%"Games\Hearthstone\Data
-	copy /y hs-data\DBF\CARD.xml "%PROJECTDIR%"Games\Hearthstone\Data
-	for /d %%p in ("hs-data") do rmdir "%%p" /s /q
+	git clone https://gitlab.com/HearthCode/DataExtractor
+	pushd DataExtractor
+	call build
+	hs-data-extractor
+	copy /y Cards.xml "%PROJECTDIR%"Games\Hearthstone\Data
+	popd
+	for /d %%p in ("DataExtractor") do rmdir "%%p" /s /q
 )
