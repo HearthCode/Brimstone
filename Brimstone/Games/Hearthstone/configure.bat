593b58772bf20f3882a20f82bce837456e4dab69
Katy Coe <djkaty@start.no>
2017-03-15 09:49:21 +0100

    Update configure scripts to work with HearthData and .NET Core

--- Brimstone/Games/Hearthstone/configure.bat
+++ Brimstone/Games/Hearthstone/configure.bat
@@ -4,11 +4,14 @@ if not exist "%PROJECTDIR%"Games\Hearthstone\Data\Cards.xml (
 	if not exist "%PROJECTDIR%"Games\Hearthstone\Data (
 		mkdir "%PROJECTDIR%"Games\Hearthstone\Data
 	)
-	git clone https://gitlab.com/HearthCode/DataExtractor
-	pushd DataExtractor
-	call build
-	hs-data-extractor
-	copy /y Cards.xml "%PROJECTDIR%"Games\Hearthstone\Data
+	git clone https://gitlab.com/HearthCode/HearthData
+	pushd HearthData
+	dotnet restore
+	dotnet run --extract-card-data="%PROJECTDIR%"Games\Hearthstone\Data\Cards.xml
 	popd
-	for /d %%p in ("DataExtractor") do rmdir "%%p" /s /q
+	for /d %%p in ("HearthData") do rmdir "%%p" /s /q
+)
+if not exist "%PROJECTDIR%"Games\Hearthstone\Data\Cards.xml (
+	echo HearthData: error %ERRORLEVEL%: Could not generate card metadata XML
+	exit /B 1
 )
